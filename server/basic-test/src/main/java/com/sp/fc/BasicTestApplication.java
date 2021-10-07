f4be76b2aead21dadfde74f65a1aad599c201be8
melon9751@naver.com <melon9751@naver.com>
2021-10-07 20:02:07 +0900

    초기 셋팅 테스트 제거
    다른 브랜치로 초기 셋팅을 복구시키기 위해 테스트를 제거합니다

--- server/basic-test/src/main/java/com/sp/fc/BasicTestApplication.java
+++ server/basic-test/src/main/java/com/sp/fc/BasicTestApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BasicTestApplication {
     public static void main(String[] args) {
-        Person person = Person.builder().name("test").build();
-        System.out.println(person);
-
-        //SpringApplication.run(BasicTestApplication.class, args);
+        SpringApplication.run(BasicTestApplication.class, args);
     }
 }
