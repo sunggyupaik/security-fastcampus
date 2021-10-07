7ea972f8aa68d31defe9dbbace3de80f027409a9
melon9751@naver.com <melon9751@naver.com>
2021-10-07 21:14:15 +0900

    필터 체인 조건 추가
    필터 체인에 모든 경로 허용을 추가하고 기본로그인을 사용한다는 내용을
    추가합니다

--- server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sp.fc.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Order(1)
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,11 +37,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**");
         http.authorizeRequests((requests) ->
                 requests.antMatchers("/")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
         );
+        http.formLogin();
+        http.httpBasic();
     }
 }
