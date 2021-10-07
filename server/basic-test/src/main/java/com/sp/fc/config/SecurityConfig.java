4d58abc4524b5c0e7a98959659ee6ecf6e3a93f4
melon9751@naver.com <melon9751@naver.com>
2021-10-07 20:46:29 +0900

    인증 무시하고 리소스 접근 허용하기
    configure 메서드를 통해 어떠한 접근도 허용하여 리소스를 열어주도록
    설정합니다

--- server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sp.fc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -30,5 +31,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) ->
+                requests.antMatchers("/")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+        );
     }
 }
