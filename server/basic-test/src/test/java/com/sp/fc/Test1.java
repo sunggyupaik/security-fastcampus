e3cb9a9a0d24f1863f120156c0fe3bc17390ea01
melon9751@naver.com <melon9751@naver.com>
2021-10-07 17:39:06 +0900

    build.gradle 내용 추가
    각 프로젝트에 필요한 gradle 설정을 추가합니다. root 경로에는 글로벌
    설정이 있고, 각 프로젝트 마다 파일이 생성됩니다

---
+++ server/basic-test/src/test/java/com/sp/fc/Test1.java
@@ -0,0 +1,12 @@
+package com.sp.fc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+    @Test
+    void test_1() {
+        assertEquals("test", "test");
+    }
+}
