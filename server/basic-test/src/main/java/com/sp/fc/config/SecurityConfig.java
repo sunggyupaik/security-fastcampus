50f6ab4e4f1664620e11db76794a6d7fb66e2c41
melon9751@naver.com <melon9751@naver.com>
2021-10-08 14:38:02 +0900

    로그인 시, csrf 토큰 설정 추가
    타임리프에서 csrf 토큰 생성 설정을 추가합니다

--- server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin(
                         login -> login.loginPage("/login")
                                 .permitAll()
+                                .defaultSuccessUrl("/", false)
                 );
     }
 
