8a34db2b147251909af12568ee1dbedd6e98ec65
melon9751@naver.com <melon9751@naver.com>
2021-10-08 09:19:05 +0900

    초기 설정 세팅
    front 화면 추가 및 요청 경로를 수정합니다. 시큐리티 필터도 간략하게
    수정합니다

--- server/basic-test/src/main/java/com/sp/fc/LoginBasicApplication.java
+++ server/basic-test/src/main/java/com/sp/fc/LoginBasicApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicTestApplication {
+public class LoginBasicApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BasicTestApplication.class, args);
+        SpringApplication.run(LoginBasicApplication.class, args);
     }
 }
--- server/basic-test/src/main/java/com/sp/fc/web/controller/HomeController.java
+++ server/basic-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -1,39 +1,42 @@
 package com.sp.fc.web.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class HomeController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
-        return "홈페이지";
+        return "index";
     }
 
-    @RequestMapping("/auth")
-    public Authentication auth() {
-        return SecurityContextHolder.getContext()
-                .getAuthentication();
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "loginForm";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "AccessDenied";
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_USER')")
-    @RequestMapping("/user")
-    public SecurityMessage user() {
-        return SecurityMessage.builder()
-                .auth(SecurityContextHolder.getContext().getAuthentication())
-                .message("User 정보")
-                .build();
+    @GetMapping("/user-page")
+    public String user() {
+        return "UserPage";
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
-    @RequestMapping("/admin")
-    public SecurityMessage admin() {
-        return SecurityMessage.builder()
-                .auth(SecurityContextHolder.getContext().getAuthentication())
-                .message("관리자 정보")
-                .build();
+    @GetMapping("/admin-page")
+    public String admin() {
+        return "AdminPage";
     }
 }
