de8be023f9efde40e052e20f777b620184bcdb88
melon9751@naver.com <melon9751@naver.com>
2021-10-08 13:34:45 +0900

    로그인 권한부여 및 계정 생성
    로그인을 위해 페이지별 권한 설정 및 web 리소스 제한 해제를 하였습니다.
    또한 계정을 인메모리에서 생성합니다

--- server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/config/SecurityConfig.java
@@ -1,11 +1,32 @@
 package com.sp.fc.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                ).withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("admin")
+                                .password("2222")
+                                .roles("ADMIN")
+                );
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -13,6 +34,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                 request
                                         .antMatchers("/").permitAll()
                                         .anyRequest().authenticated();
-                });
+                })
+                .formLogin(
+                        login -> login.loginPage("/login")
+                                .permitAll()
+                );
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
     }
 }
