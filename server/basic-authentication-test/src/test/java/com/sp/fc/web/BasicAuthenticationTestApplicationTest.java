11300c6864f9610d7060e8e21bb6936587c9dba0
melon9751@naver.com <melon9751@naver.com>
2021-10-11 00:37:32 +0900

    초기 설정 추가
    SPA를 위한 basic 로그인 초기 설정을 추가합니다

---
+++ server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTestApplicationTest.java
@@ -0,0 +1,8 @@
+package com.sp.fc.web;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BasicAuthenticationTestApplicationTest {
+
+}
