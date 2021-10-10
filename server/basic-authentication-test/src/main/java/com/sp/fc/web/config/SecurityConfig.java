c4c3182ced74d3e78a489ee09c4b3a33fa017da7
melon9751@naver.com <melon9751@naver.com>
2021-10-11 00:44:50 +0900

    루트 경로 조회 요청 테스트 추가
    초기 루트경로를 만들고 일반 요청에 성공하는 테스트를 추가합니다

--- server/basic-authentication-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-authentication-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -9,7 +9,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests().anyRequest().permitAll() //.authenticated()
                 .and()
                 .httpBasic();
     }
--- server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTestApplicationTest.java
+++ server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTestApplicationTest.java
@@ -1,8 +1,24 @@
 package com.sp.fc.web;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BasicAuthenticationTestApplicationTest {
+    @LocalServerPort
+    int port;
 
+    RestTemplate client = new RestTemplate();
+
+    private String greetingUrl() {
+        return "http://localhost:" + port + "/greeting";
+    }
+
+    @Test
+    void test() {
+        String response = client.getForObject(greetingUrl(), String.class);
+        System.out.println(response);
+    }
 }
