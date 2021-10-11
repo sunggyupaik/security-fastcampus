d46990f988411b3c2008dc38aac598ee06afb662
melon9751@naver.com <melon9751@naver.com>
2021-10-11 10:31:01 +0900

    초기 프로젝트 설정
    login-custom-filter의 브랜치 내용을 가져와서 초기 프로젝트를 셋팅합니다

---
+++ server/login-multi-chain/src/main/java/com/sp/fc/web/student/Student.java
@@ -0,0 +1,15 @@
+package com.sp.fc.web.student;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+@Data
+@Builder
+public class Student {
+    private String id;
+    private String username;
+    private Set<GrantedAuthority> role;
+}
