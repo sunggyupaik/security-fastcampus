38c64d7a5c12926ed3b1020ab0a10c59c4b95105
sunggyupaik <melon9751@naver.com>
2021-10-15 17:48:31 +0900

    accessDecisionManager 커스터마이징
    권한 인증을 커스터마이징할 수 있도록 구현을 추가합니다. 모든 경로에 대해
    검사하며 통과하게 합니다

--- server/authority-test/src/main/java/com/sp/fc/config/SecurityConfig.java
+++ server/authority-test/src/main/java/com/sp/fc/config/SecurityConfig.java
@@ -1,14 +1,26 @@
 package com.sp.fc.config;
 
 
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+
+import java.util.Collection;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    FilterSecurityInterceptor filter;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -20,14 +32,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 );
     }
 
+    AccessDecisionManager filterAccessDecisionManager() {
+        return new AccessDecisionManager() {
+            @Override
+            public void decide(Authentication authentication, Object object,
+                               Collection<ConfigAttribute> configAttributes)
+                    throws AccessDeniedException, InsufficientAuthenticationException {
+//                throw new AccessDeniedException("접근 금지");
+                return;
+            }
+
+            @Override
+            public boolean supports(ConfigAttribute attribute) {
+                return true;
+            }
+
+            @Override
+            public boolean supports(Class<?> clazz) {
+                return FilterInvocation.class.isAssignableFrom(clazz);
+            }
+        };
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .httpBasic()
                 .and()
-                .authorizeRequests(authority->
-                        authority.anyRequest().authenticated()
+                .authorizeRequests(
+                        authority-> authority
+                            .mvcMatchers("/greeting").hasRole("ADMIN")
+                            .anyRequest().authenticated()
+                            .accessDecisionManager(filterAccessDecisionManager())
                 );
     }
 }
