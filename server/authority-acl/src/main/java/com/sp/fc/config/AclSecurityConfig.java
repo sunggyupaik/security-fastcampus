b012e517d9e8c608160ad9fdf8f772ee8ddeb03e
sunggyupaik <melon9751@naver.com>
2021-10-22 17:17:15 +0900

    시큐리티 설정을 추가하라
    기본적인 로그인 설정과 인메모리 계정을 생성합니다

--- server/authority-acl/src/main/java/com/sp/fc/config/AclSecurityConfig.java
+++ server/authority-acl/src/main/java/com/sp/fc/config/AclSecurityConfig.java
@@ -8,5 +8,29 @@ import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity
 public class AclSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser(
+						User.withDefaultPasswordEncoder()
+								.username("student1")
+								.password("1111")
+								.roles("STUDENT")
+				)
+				.withUser(
+						User.withDefaultPasswordEncoder()
+								.username("tutor1")
+								.password("1111")
+								.roles("TUTOR")
+				);
+	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests(
+						request->request.anyRequest().authenticated()
+				)
+				.httpBasic();
+	}
 }
