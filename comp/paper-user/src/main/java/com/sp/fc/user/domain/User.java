8b9f7b9e332bf6b3b4e5ba3c0c65ee816dafbc0b
sunggyupaik <melon9751@naver.com>
2021-10-23 10:15:25 +0900

    사용자, 권한 및 서비스를 추가하라
    사용자, 권한, 서비스를 추가하고 테스트를 추가합니다

---
+++ comp/paper-user/src/main/java/com/sp/fc/user/domain/User.java
@@ -0,0 +1,81 @@
+package com.sp.fc.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="sp_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(foreignKey = @ForeignKey(name = "userId"))
+    private Set<Authority> authorities;
+
+    private String grade;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User teacher;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private School school;
+
+    private boolean enabled;
+
+    @Column(updatable = false)
+    private LocalDateTime created;
+
+    private LocalDateTime updated;
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return enabled;
+    }
+
+}
